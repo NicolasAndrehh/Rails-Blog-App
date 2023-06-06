90587abfc67f99c2074debeaf4a3e8f3229e2d56
Nicolas Olaya <111377816+NicolasAndrehh@users.noreply.github.com>
2023-06-06 15:56:48 -0500

    Fix Rubocop errors

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,10 +1,10 @@
-require_relative 'application_record.rb'
+require_relative 'application_record'
 
 class Comment < ApplicationRecord
-    belongs_to :user, optional: true
-    belongs_to :post, optional: true
+  belongs_to :user, optional: true
+  belongs_to :post, optional: true
 
-    def update_comment_counter
-        Post.find(post_id).update(comments_counter: Comment.where(post_id: post_id).count)
-    end
+  def update_comment_counter
+    Post.find(post_id).update(comments_counter: Comment.where(post_id:).count)
+  end
 end
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,8 +1,8 @@
 class Like < ApplicationRecord
-    belongs_to :user, optional: true
-    belongs_to :post, optional: true
+  belongs_to :user, optional: true
+  belongs_to :post, optional: true
 
-    def update_like_counter
-        Post.find(post_id).update(likes_counter: Like.where(post_id: post_id).count)
-    end
+  def update_like_counter
+    Post.find(post_id).update(likes_counter: Like.where(post_id:).count)
+  end
 end
