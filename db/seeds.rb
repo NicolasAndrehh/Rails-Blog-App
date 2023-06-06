a35884ee11fe0cbb37c1c42494f26a20b82b3dec
Nicolas Olaya <111377816+NicolasAndrehh@users.noreply.github.com>
2023-06-06 15:53:00 -0500

    Add update_like_counter method to Like

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
-
 # Create users
 first_user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
 second_user = User.create(name: 'Lilly', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Poland.')
@@ -23,3 +15,7 @@ Comment.create(post_id: first_post.id, author_id: second_user.id, text: 'Hi Tom!
 Comment.create(post_id: first_post.id, author_id: first_user.id, text: 'Hi Lilly!' )
 Comment.create(post_id: first_post.id, author_id: second_user.id, text: 'Hi Tom!' )
 Comment.create(post_id: first_post.id, author_id: first_user.id, text: 'Hi Lilly!' )
+
+# Create likes
+Like.create(post_id: first_post.id, user_id: first_user.id)
+Like.create(post_id: first_post.id, user_id: second_user.id)
