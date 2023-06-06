6786527ef5df7c5db7413dff07c9a4a6531924e3
Nicolas Olaya <111377816+NicolasAndrehh@users.noreply.github.com>
2023-06-06 14:40:44 -0500

    Add seed to populate the db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# Create users
+first_user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
+second_user = User.create(name: 'Lilly', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Poland.')
+
+# Create posts
+first_post = Post.create(author_id: first_user.id, title: 'Hello', text: 'This is my first post')
+second_post = Post.create(author_id: first_user.id, title: 'Hello', text: 'This is my second post')
+third_post = Post.create(author_id: first_user.id, title: 'Hello', text: 'This is my third post')
+fourth_post = Post.create(author_id: first_user.id, title: 'Hello', text: 'This is my fourth post')
+
+# Create comments
+Comment.create(post_id: first_post.id, author_id: second_user.id, text: 'Hi Tom!' )
+Comment.create(post_id: first_post.id, author_id: first_user.id, text: 'Hi Lilly!' )
+Comment.create(post_id: first_post.id, author_id: second_user.id, text: 'Hi Tom!' )
+Comment.create(post_id: first_post.id, author_id: first_user.id, text: 'Hi Lilly!' )
+Comment.create(post_id: first_post.id, author_id: second_user.id, text: 'Hi Tom!' )
+Comment.create(post_id: first_post.id, author_id: first_user.id, text: 'Hi Lilly!' )
