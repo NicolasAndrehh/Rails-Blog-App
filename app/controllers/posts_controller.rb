c5d84ec423e48446e727e1fb8ec268d397f7b37e
Nicolas Olaya <111377816+NicolasAndrehh@users.noreply.github.com>
2023-06-12 19:18:16 -0500

    Create post index view with basic styles

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,10 @@
 class PostsController < ApplicationController
   def index
-    # @posts = Post.all
+    @user = User.find(params[:user_id])
+    @posts = Post.where(author_id: @user.id).order(created_at: :desc)
   end
 
   def show
-    # @post = Post.find(params[:id])
+    @post = Post.find(params[:id])
   end
 end
