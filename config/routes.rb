b65f6c6c8cf2e2ae4439d45d8cb849af8e0eb80b
NicolasAndrehh <nicolasolaya22@gmail.com>
2023-06-21 15:01:07 -0500

    Set root route to login form

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "users#index"
+  devise_scope :user do
+    root "devise/sessions#new"
+  end
 
   resources :users, only: [:index, :show] do 
     resources :posts do 
